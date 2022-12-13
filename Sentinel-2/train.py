3ac29da21db39cd9ec4d6da408853f6e87e89692
Paul Hoehn <paul-hoehn@outlook.de>
2022-12-13 09:01:23 +0000

    Adding k fold cross validation

--- Sentinel-2/train.py
+++ Sentinel-2/train.py
@@ -3,6 +3,7 @@ from glob import glob
 import yaml
 from tools_model import dice_coef, load_img_as_array
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from unet import binary_unet
 from matplotlib import pyplot as plt
 from datagen import CustomImageGenerator
@@ -24,6 +25,7 @@ if os.path.exists("config.yaml"):
         epochs = data['model_parameter']['epochs']
         indizes = data["indizes"]
         output_folder = data["output_folder"]
+        k_folds = data['model_parameter']['kfold']
 
         seed = data["seed"]
 
@@ -39,76 +41,111 @@ else:
     mask_list = glob("{}/crops/no_idx/mask/*.tif".format(output_folder))
     model_name = parameter[optimizer] + "_" + parameter[loss_function] + "_" + str(epochs) + "no_idx" # Ad_bc_100
 
+def get_model_name(k):
+    return model_name + "_" + str(k) + '.h5'
+
 model_path = os.path.join(output_folder, "models", model_name)
 
 img_list.sort()
 mask_list.sort()
 
-import pdb 
-pdb.set_trace()
-
 # Split training data
+# X_train, X_test, y_train, y_test = train_test_split(img_list, mask_list, test_size = 0.20, shuffle=True, random_state = seed)
+# X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = 0.10, shuffle=True, random_state = seed)
+
+# Split training data in training-test set 
 X_train, X_test, y_train, y_test = train_test_split(img_list, mask_list, test_size = 0.20, shuffle=True, random_state = seed)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = 0.10, shuffle=True, random_state = seed)
+print(f"Training data: {len(X_train)} Test dataset: {len(X_test)}")
 
 # Load images and masks with an custom data generator - for performance reason
 patch_array = load_img_as_array(X_train[0])
 patch_xy = (patch_array.shape[0], patch_array.shape[1])
 b_count = patch_array.shape[-1]
 
-train_datagen = CustomImageGenerator(X_train, y_train, patch_xy, b_count)
-val_datagen = CustomImageGenerator(X_val,y_val, patch_xy, b_count)
 test_datagen = CustomImageGenerator(X_test, y_test, patch_xy, b_count)
 
-# sanity check
-# batch_nr = random.randint(0, len(train_datagen))
-# X,y = train_datagen[batch_nr]
+## Training with K-fold cross validation
+kf = KFold(n_splits=k_folds, random_state=seed, shuffle=True)
+fold_var = 1
+val_bn_acc = []
+val_recall = []
+val_precision = []
+val_bn_iou = []
 
-# for i in range(X.shape[0]):
+for i, (train_index, val_index) in enumerate(kf.split(X_train)):
+    print(f"Fold {i}:")
+    print(f"  Train: index={train_index} Length: {len(train_index)}")
+    print(f"  Validation:  index={val_index} Length: {len(val_index)}")
 
-#     plt.figure(figsize=(12,6))
-#     plt.subplot(121)
-#     plt.imshow(X[i][:,:,4])
-#     plt.subplot(122)
-#     plt.imshow(y[i])
-#     plt.show()
-#     plt.savefig("sanity_check{}.png".format(i)) 
-                                                                                                                                                                                                                                                                                                                                                        
-#Load model
-model = binary_unet(patch_xy[0], patch_xy[1], b_count)  
+    # List of paths to images based on train_index
+    X_train_cv = [X_train[idx] for idx in train_index]
+    y_train_cv = [y_train[idx] for idx in train_index]
+    X_val_cv = [X_train[idx] for idx in val_index]
+    y_val_cv = [y_train[idx] for idx in val_index]
+    
+    train_datagen = CustomImageGenerator(X_train_cv, y_train_cv, patch_xy, b_count)
+    val_datagen = CustomImageGenerator(X_val_cv, y_val_cv, patch_xy, b_count)
 
-# metrics 
-model.compile(optimizer=optimizer, loss=loss_function, metrics=[dice_coef,
-                    tf.keras.metrics.BinaryAccuracy(name ="accuracy"),
-                    tf.keras.metrics.Recall(name="recall"),
-                    tf.keras.metrics.Precision(name="precision"),
-                    tf.keras.metrics.BinaryIoU(name="iou")])
+    # sanity check
+    # X,y = train_datagen[0]
 
-# Model fit 
-log_dir = os.path.join(output_folder, "models", "logs", model_name) 
-checkpoint_path = os.path.join(output_folder, "models", "checkpoints", model_name)
-#tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
-checkpoint_callback = ModelCheckpoint(checkpoint_path, monitor="val_iou", mode='max', verbose=1, save_best_only=True, save_weights_only=True)
+    # for i in range(X.shape[0]):
 
-model.fit(train_datagen, validation_data=val_datagen, verbose=1, epochs=epochs, callbacks=[checkpoint_callback])
+    #     plt.figure(figsize=(12,6))
+    #     plt.subplot(121)
+    #     plt.imshow(X[i][:,:,2:5])
+    #     plt.subplot(122)
+    #     plt.imshow(y[i])
+    #     plt.show()
+    #     plt.savefig("sanity_check{}.png".format(i)) 
+    
+    #Load model
+    model = binary_unet(patch_xy[0], patch_xy[1], b_count)  
 
-# Save model for prediction
-model.save(model_path)
+    # metrics 
+    model.compile(optimizer=optimizer, loss=loss_function, metrics=[dice_coef,
+                        tf.keras.metrics.BinaryAccuracy(name ="accuracy"),
+                        tf.keras.metrics.Recall(name="recall"),
+                        tf.keras.metrics.Precision(name="precision"),
+                        tf.keras.metrics.BinaryIoU(name="iou")])
 
-model.load_weights(checkpoint_path)
-model.evaluate(test_datagen)
+    # Model fit 
+    #log_dir = os.path.join(output_folder, "models", "logs", model_name) 
+    #tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
 
-pred_test = model.predict(test_datagen) # f.eg.(288,128,128,1)
-pred_test = (pred_test > 0.5).astype(np.uint8) 
+    checkpoint_path = os.path.join(output_folder, "models", "checkpoints", get_model_name(fold_var))
+    checkpoint_callback = ModelCheckpoint(checkpoint_path, monitor="val_iou", mode='max', verbose=1, save_best_only=True, save_weights_only=True)
 
-for i in range((test_datagen[0][1].shape[0])):
+    history = model.fit(train_datagen, validation_data=val_datagen, verbose=1, epochs=epochs, callbacks=[checkpoint_callback])
 
-    plt.figure(figsize=(12,6))
-    plt.subplot(121)
-    plt.imshow(test_datagen[0][1][i])
-    plt.subplot(122)
-    plt.imshow(pred_test[i])
-    plt.show()
-    plt.savefig("/home/hoehn/data/prediction/prediction{}.png".format(i))
+    # Save model for prediction
+    # model.save(model_path)
 
+    model.load_weights(checkpoint_path)
+    
+    results = model.evaluate(val_datagen)
+    results = dict(zip(model.metrics_names,results))
+    
+    val_bn_acc.append(results['accuracy'])
+    val_recall.append(results['recall'])
+    val_precision.append(results['precision'])
+    val_bn_iou.append(results['iou']) 
 
+    #pred_test = model.predict(test_datagen) # f.eg.(288,128,128,1)
+    print("Test dataset evaluation: ")
+    pred_results = model.evaluate(test_datagen)
+    #pred_test = (pred_test > 0.5).astype(np.uint8) 
+
+    # for i in range((test_datagen[0][1].shape[0])):
+
+    #     plt.figure(figsize=(12,6))
+    #     plt.subplot(121)
+    #     plt.imshow(test_datagen[0][1][i])
+    #     plt.subplot(122)
+    #     plt.imshow(pred_test[i])
+    #     plt.show()
+    #     plt.savefig("/home/hoehn/data/prediction/prediction{}.png".format(i))
+
+    tf.keras.backend.clear_session()
+
+    fold_var += 1
